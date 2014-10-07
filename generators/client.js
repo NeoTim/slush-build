362449bda3db6b108de821e9c2160cd321785363
joelcoxokc <joelcox@hisimagination.com>
2014-10-07 13:38:24 -0700

    Update Fix issues with build.json

--- generators/client.js
+++ generators/client.js
@@ -3,11 +3,16 @@ var es = require('event-stream');
 module.exports = function(gulp, g, install, conflict, template, rename, _, inflection, inquirer, mkdirp){
   gulp.task('client', function (done) {
 
-    gulp.src('bower.json')
+    gulp.src('build.json')
       .pipe( g.jsonEditor( function ( json ) {
+        json.clientDir = './client';
         console.log(json)
+        return json;
       } ) )
       .pipe( gulp.dest( '.' ) )
+
+    /* Pipe the app/ dir to the client/ dir */
+    gulp.src
       .on('end', function(){
         done();
       });
