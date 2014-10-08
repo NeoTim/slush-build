f4ee7d17a9ef59ba75cd229b80f83d5e94b3151e
joelcoxokc <joelcox@hisimagination.com>
2014-10-08 10:04:14 -0700

    Update to Core

---
+++ templates/beginner/templates/app/core/header/header.controller.js
@@ -0,0 +1,12 @@
+;(function(){
+  'use struct';
+  angular
+    .module('app')
+    .controller('HeaderCtrl', HeaderCtrl);
+    /* @inject */
+    function HeaderCtrl($scope, $location, $state, StatesService) {
+
+
+    }
+
+}).call(this);
---
+++ templates/beginner/templates/app/core/menu/menu.controller.js
@@ -0,0 +1,12 @@
+;(function(){
+  'use struct';
+  angular
+    .module('app')
+    .controller('MenuCtrl', MenuCtrl);
+    /* @inject */
+    function MenuCtrl($scope, $location, $state, StatesService) {
+
+
+    }
+
+}).call(this);
